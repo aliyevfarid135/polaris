ce4a66bf0f158f200c3a3b0ff749cae9e015b569
Farid Aliyev <142145981+aliyevfarid135@users.noreply.github.com>
2025-03-26 00:19:26 +0400

    Update CustomUserDetailsServiceImpl.java

--- src/main/java/com/polaris/service/CustomUserDetailsServiceImpl.java
+++ src/main/java/com/polaris/service/CustomUserDetailsServiceImpl.java
@@ -30,15 +30,12 @@ public class CustomUserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         System.out.println("Attempting to find user with email: " + username);
-
-        // Find the user by email
+        
         User user = userRepository.findByEmail(username);
 
-        // Check if the user is a mentor
         Optional<Mentor> mentor = mentorRepository.findByUser(user);
 
         if (user != null) {
-            // Determine the role based on whether the user is a mentor
             String role = (mentor != null) ? "ROLE_MENTOR" : "ROLE_LEARNER";
 
             System.out.println("Found user: " + user.getEmail() + " with role: " + role);
